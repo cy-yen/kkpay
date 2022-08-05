b3e1753e4353fa96d721dd2397ece00d9d28c627
jimmy <jimmy@888>
2022-08-05 14:37:50 +0800

    first commit

---
+++ js/swiper-3.4.2.min.js
@@ -0,0 +1,270 @@
+<!doctype html>
+<html>
+
+<head>
+	<meta charset="utf-8">
+
+	<title>KKPAY</title>
+	<link rel="stylesheet" href="css/swiper-3.4.2.min.css" type="text/css">
+	<link rel="stylesheet" href="css/animate.min.css" type="text/css">
+	<link rel="stylesheet" href="css/styey1.css" type="text/css">
+	<link rel="icon" href="./images/favicon.ico">
+	<script src="js/jquery-1.12.4.min.js"></script>
+	<script src="js/swiper-3.4.2.min.js"></script>
+	<script src="js/swiper.animate1.0.2.min.js"></script>
+	<script src="js/wow.min.js" type="text/javascript"></script>
+	<script>
+		new WOW().init(0);
+	</script>
+</head>
+
+<body>
+	<div class="header">
+		<div class="headerbox">
+			<h1 class="logo"><a href=""><img src="images/p_03.png" /></a></h1>
+			<div class="nav">
+				<ul>
+					<li class="shouye"><a href="index.html">首页</a></li>
+					<li class="nav2"><a href="javascript:void(0);">产品介绍</a></li>
+					<li><a href="jc_p.html">教程</a></li>
+					<li><a href="xzr_p.html">须知</a></li>
+				</ul>
+			</div>
+		</div>
+	</div>
+	<div class="tpbj">
+		<div class="wz1285">
+			<div class="zyzf">
+				<div class="zyzf_left wow fadeInLeft" data-wow-delay="0.4s">
+					<div class="zytop">
+						<img src="images/p_10.png" />
+						<h2>助您安全、自由支付</h2>
+						<p>kk币 - 世界通用数字资产，恒定人民币汇率 1:1，100%互兑 无需远行即可畅玩全球</p>
+					</div>
+					<div class="rwm">
+						<p>请使用手机扫码下方二维码开始下载APP</p>
+						<img src="images/p_25.png" />
+					</div>
+				</div>
+				<div class="zyzf_right wow fadeInRight" data-wow-delay="0.3s">
+					<div class="tpa"><img src="images/s_49.png" /></div>
+					<div class="tpb"><img src="images/s_50.png" /></div>
+					<img src="images/p_07.png" />
+					<ul>
+						<li><a href="">买币</a></li>
+						<li><a href="">卖币</a></li>
+					</ul>
+				</div>
+			</div>
+			<div class="vdjc" id="section-2">
+				<div class="vdjc_tit wow fadeInUp" data-wow-delay="0.3s">
+					<h2>视频教程</h2>
+				</div>
+				<div class="wdjc_nr">
+					<ul>
+						<li class=" wow fadeInLeft" data-wow-delay="0.4s">
+							<div class="wdbox"><img src="images/p_34.jpg" /><span class="dk1"></span>
+								<p>KKPAY操作教程</p>
+							</div>
+						</li>
+						<li class=" wow fadeInRight" data-wow-delay="0.3s">
+							<div class="wdbox"><img src="images/p_36.jpg" /><span class="dk2"></span>
+								<p>娱乐平台存取款教程</p>
+							</div>
+						</li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="wz1285">
+		<div class="qszf">
+			<div class="zfbt_tit wow fadeInUp" data-wow-delay="0.3s">
+				<h2>轻松支付</h2>
+				<p>使用kkpay，您可以轻松购买kk币，并存入您的kkpay账户。您可以随时使用kk币支付给受支持的商家。<br />我们在全世界已有1000+合作商家，现在就使用kkpay畅享缤纷服务吧</p>
+			</div>
+			<div class="zfnr">
+				<ul>
+					<li class=" wow fadeInUp" data-wow-delay="0.3s">
+						<div class="zfbox1">
+							<img src="images/p_40.png" />
+							<p>STEP1：<br />电子支付购买kk币</p>
+						</div>
+					</li>
+					<li class=" wow fadeInUp" data-wow-delay="0.4s">
+						<div class="zfbox2">
+							<h3>购买</h3>
+							<img src="images/p_47.jpg" />
+						</div>
+					</li>
+					<li class=" wow fadeInUp" data-wow-delay="0.5s">
+						<div class="zfbox1">
+							<img src="images/p_42.png" />
+							<p>STEP2：<br />购买kk币存入kkpay账户</p>
+						</div>
+					</li>
+					<li class=" wow fadeInUp" data-wow-delay="0.6s">
+						<div class="zfbox2">
+							<h3>购买</h3>
+							<img src="images/p_47.jpg" />
+						</div>
+					</li>
+					<li class=" wow fadeInUp" data-wow-delay="0.7s">
+						<div class="zfbox1">
+							<img src="images/p_44.png" />
+							<p>STEP3：<br />随时使用kk币轻松支付给商家</p>
+						</div>
+					</li>
+				</ul>
+			</div>
+		</div>
+		<div class="qszf">
+			<div class="zfbt_tit wow fadeInUp" data-wow-delay="0.3s">
+				<h2>随时兑现</h2>
+				<p>您从商家提取kk币至您的kkpay账户后，无论何时，您都可以打开您的kkpay，将kk币出售，并获得人民币。<br />我们提供极速、安全的交易渠道。不仅保障您能快速获取人民币，且非常重视您的银行账户安全，严控每一笔资金来源
+				</p>
+			</div>
+			<div class="zfnr">
+				<ul>
+					<li class="wow fadeInUp" data-wow-delay="0.3s">
+						<div class="zfbox1">
+							<img src="images/p_54.png" />
+							<p>STEP4：<br />从商家提取kk币至kkpay账户</p>
+						</div>
+					</li>
+					<li class="wow fadeInUp" data-wow-delay="0.4s">
+						<div class="zfbox2">
+							<h3>提取</h3>
+							<img src="images/p_47.jpg" />
+						</div>
+					</li>
+					<li class="wow fadeInUp" data-wow-delay="0.5s">
+						<div class="zfbox1">
+							<img src="images/p_57.png" />
+							<p>STEP5：<br />从kkpay账户卖出kk币</p>
+						</div>
+					</li>
+					<li class="wow fadeInUp" data-wow-delay="0.6s">
+						<div class="zfbox2">
+							<h3>销售</h3>
+							<img src="images/p_47.jpg" />
+						</div>
+					</li>
+					<li class="wow fadeInUp" data-wow-delay="0.7s">
+						<div class="zfbox1">
+							<img src="images/p_60.png" />
+							<p>STEP6：<br />获得人民币</p>
+						</div>
+					</li>
+				</ul>
+			</div>
+		</div>
+	</div>
+	<div class="footer">
+		<div class="footerbox">
+			<div class="foot_left">
+				<p><img src="images/p_69.png" />扫码二维码进行下载</p>
+			</div>
+			<div class="foot_right"><img src="images/p_66.png" /></div>
+		</div>
+	</div>
+	<div class="jcbj">
+
+	</div>
+	<div class="jctc1">
+		<video src="images/vd1.mp4" width="100%" controls="controls"></video>
+		<span class="ggbtn"></span>
+	</div>
+	<div class="jctc2">
+		<video src="images/vd2.mp4" width="100%" controls="controls"></video>
+		<span class="ggbtn1"></span>
+	</div>
+	<script type="text/javascript">
+		function fn(a, b) {
+			a.click(function () {
+				$('html,body').animate({ scrollTop: b.offset().top - 100 }, 800);
+			});
+		}
+		fn($('.nav1'), $('#section-1'));
+		fn($('.nav2'), $('#section-2'));
+		fn($('.nav3'), $('#section-3'));
+		$(".shouye").click(function () {
+			window.localStorage.setItem('another', false)
+		})
+	</script>
+	<script type="text/javascript">
+		$('.dk1').click(function () {
+			$('.jcbj').fadeIn();
+			$('.jctc1').fadeIn();
+		})
+		$('.ggbtn').click(function () {
+			$('.jcbj').fadeOut();
+			$('.jctc1').fadeOut();
+			$('video').trigger('pause');
+			$('video')[0].trigger('pause');
+			$('video').get(0).trigger('pause');
+		})
+		$('.dk2').click(function () {
+			$('.jcbj').fadeIn();
+			$('.jctc2').fadeIn();
+		})
+		$('.ggbtn1').click(function () {
+			$('.jcbj').fadeOut();
+			$('.jctc2').fadeOut();
+			$('video').trigger('pause');
+			$('video')[1].trigger('pause');
+			$('video').get(1).trigger('pause');
+		})
+	</script>
+	<div class="jctc2"></div>
+	<script type="text/javascript">
+		function getQuerys(e) {
+			if (!e) return "";
+			var t = {},
+				r = [],
+				n = "",
+				a = "";
+			try {
+				var i = [];
+				if (e.indexOf("?") >= 0 && (i = e.substring(e.indexOf("?") + 1, e.length).split("&")), i.length > 0) for (var o in i) n = (r = i[o].split("="))[0],
+					a = r[1],
+					t[n] = a
+			} catch (s) {
+				t = {}
+			}
+			return t
+		};
+		let url = window.location.href
+		let queryData = getQuerys(decodeURIComponent(url));
+		$(window).resize(function () {
+			var screenWidth = $(window).width();
+			if (screenWidth > 1000) {
+
+			} else {
+				if (queryData.nav) {
+					window.location.href = "index_s.html?nav=1"
+				} else {
+					window.location.href = "index_s.html"
+				}
+			}
+		})
+		$(function () {
+			var screenWidth = $(window).width();
+			if (screenWidth > 1000) {
+				console.log('pc')
+			} else {
+				if (queryData.nav) {
+					window.location.href = "index_s.html?nav=1"
+				} else {
+					window.location.href = "index_s.html"
+				}
+			}
+		})
+		let bool = window.localStorage.getItem('another') || false
+		if (bool == "true") {
+			$('.nav2').click()
+		}
+	</script>
+</body>
+
+</html>
